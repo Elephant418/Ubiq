8f20a698dadd9e9ce665360fe01c6f66da307a94
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-10 23:00:48 +0100

    Add must_valid_schema tests & docs

--- doc/array/must_valid_schema.md
+++ doc/array/must_valid_schema.md
@@ -20,4 +20,55 @@ Returns TRUE if the array is conform or it has could be transformed to be confor
 Examples
 --------
 
-Todo.
+### Example 1
+
+```php
+$array = [ ];
+\UArray\must_valid_schema( $array, [ 'needed' ] );
+```
+Returns FALSE
+
+### Example 2
+
+```php
+$array = [ ];
+\UArray\must_valid_schema( $array, [ 'optional' => 3 ] );
+```
+Returns TRUE;
+$array value is [ 'optional' => 3 ]
+
+### Example 3
+
+```php
+$array = [ 'extra' => 4 ];
+\UArray\must_valid_schema( $array, [ 'optional' => 3 ] );
+```
+Returns TRUE;
+$array value is [ 'optional' => 3, 'extra' => 4 ]
+
+### Example 4
+
+```php
+$array = [ 'needed' => 1 ];
+\UArray\must_valid_schema( $array, [ 'needed' ] );
+```
+Returns TRUE;
+$array value is [ 'needed' => 1 ]
+
+### Example 5
+
+```php
+$array = [ 'needed' => 1, 'optional' => 2 ];
+\UArray\must_valid_schema( $array, [ 'needed', 'optional' => 3 ] );
+```
+Returns TRUE;
+$array value is [ 'needed' => 1, 'optional' => 2 ]
+
+### Example 6
+
+```php
+$array = [ 'needed' => 1, 'optional' => 2, 'extra' => 4 ];
+\UArray\must_valid_schema( $array, [ 'needed', 'optional' => 3 ] );
+```
+Returns TRUE;
+$array value is [ 'needed' => 1, 'optional' => 2, 'extra' => 4 ]
--- test/Ubiq/Array_Test.php
+++ test/Ubiq/Array_Test.php
@@ -23,6 +23,56 @@ class Array_Test extends \PHPUnit_Framework_TestCase {
 
 
 
+	// MUST VALID SCHEMA
+	public function test_must_valid_schema__ko( ) {
+		$array  = [ ];
+		$schema = [ 'needed' ];
+		$test = \UArray\must_valid_schema( $array, $schema );
+		$this->assertFalse( $test );
+	}
+
+	public function test_must_valid_schema__optional( ) {
+		$array  = [ ];
+		$schema = [ 'optional' => 3 ];
+		$test = \UArray\must_valid_schema( $array, $schema );
+		$this->assertTrue( $test );
+		$this->assertEquals( $array, $schema );
+	}
+
+	public function test_must_valid_schema__extra( ) {
+		$array  = [ 'extra' => 4 ];
+		$schema = [ 'optional' => 3 ];
+		$test = \UArray\must_valid_schema( $array, $schema );
+		$this->assertTrue( $test );
+		$this->assertEquals( $array, [ 'optional' => 3, 'extra' => 4 ] );
+	}
+
+	public function test_must_valid_schema__needed( ) {
+		$array  = [ 'needed' => 1 ];
+		$schema = [ 'needed' ];
+		$test = \UArray\must_valid_schema( $array, $schema );
+		$this->assertTrue( $test );
+		$this->assertEquals( $array, [ 'needed' => 1 ] );
+	}
+
+	public function test_must_valid_schema__needed_optional( ) {
+		$array  = [ 'needed' => 1, 'optional' => 2 ];
+		$schema = [ 'needed', 'optional' => 3 ];
+		$test = \UArray\must_valid_schema( $array, $schema );
+		$this->assertTrue( $test );
+		$this->assertEquals( $array, [ 'needed' => 1, 'optional' => 2 ] );
+	}
+
+	public function test_must_valid_schema__needed_optional_extra( ) {
+		$array  = [ 'needed' => 1, 'optional' => 2, 'extra' => 4 ];
+		$schema = [ 'needed', 'optional' => 3 ];
+		$test = \UArray\must_valid_schema( $array, $schema );
+		$this->assertTrue( $test );
+		$this->assertEquals( $array, [ 'needed' => 1, 'optional' => 2, 'extra' => 4 ] );
+	}
+
+
+
 	// MERGE UNIQUE
 	public function test_merge_unique__two( ) {
 		$merged = \UArray\merge_unique( [ 1, 2, 3 ], [ 2, 3, 4 ] );
