dd35ec37d1f9d1ec8d77f92233d50a8e82459d32
Thomas ZILLIOX <thomas@zilliox.me>
2013-01-10 23:39:23 +0100

    Add get_attribute_names tests & docs

--- doc/object/get_attribute_names.md
+++ doc/object/get_attribute_names.md
@@ -16,4 +16,24 @@ Returns the name of the public attributes of the passed class.
 Examples
 --------
 
-Todo.
+### Example 1
+
+```php
+class Example_Class {
+	public $name;
+	public $height;
+}
+\UObject\get_attribute_names( new Example_Class );
+```
+Returns [ 'name', 'height' ]
+
+### Example 2
+
+```php
+class Example_Class {
+	public $name;
+	public $height;
+}
+\UObject\get_attribute_names( 'Example_Class' );
+```
+Returns [ 'name', 'height' ]
--- test/Ubiq/Object_Test.php
+++ test/Ubiq/Object_Test.php
@@ -8,7 +8,7 @@ class Object_Test extends \PHPUnit_Framework_TestCase {
 
 
 
-	// MUST BE ARRAY
+	// MUST BE CLASS
 	public function test_must_be_class__object( ) {
 		$class = New \Exception;
 		\UObject\must_be_class( $class );
@@ -20,4 +20,22 @@ class Object_Test extends \PHPUnit_Framework_TestCase {
 		\UObject\must_be_class( $class );
 		$this->assertEquals( $class, '\\Exception' );
 	}
+
+
+
+	// GET ATTRIBUTE NAMES
+	public function test_get_attribute_names__object( ) {
+		$attributes = \UObject\get_attribute_names( new Example_Class );
+		$this->assertEquals( $attributes, [ 'name', 'height' ] );
+	}
+
+	public function test_get_attribute_names__class( ) {
+		$attributes = \UObject\get_attribute_names( '\\' . __NAMESPACE__ . '\\Example_Class' );
+		$this->assertEquals( $attributes, [ 'name', 'height' ] );
+	}
+}
+
+class Example_Class {
+	public $name;
+	public $height;
 }
